bacc7d5aefab58ac77b557b1ccc266e9470d2a22
albertllopart <d.pocafeiner@gmail.com>
2018-10-07 16:56:07 +0200

    assimp.lib added
    also fixed an issue with mesh struct created in two different files

--- Project/ModuleSceneEditor.h
+++ Project/ModuleSceneEditor.h
@@ -6,17 +6,6 @@
 
 #include <list>
 
-struct Mesh
-{
-	uint id_index = 0; // index in VRAM
-	uint num_index = 0;
-	uint* index = nullptr;
-
-	uint id_vertex = 0; // unique vertex in VRAM
-	uint num_vertex = 0;
-	float* vertex = nullptr;
-};
-
 class ModuleSceneEditor : public Module
 {
 public:
