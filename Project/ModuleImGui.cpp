9e2e37f27dfe4661d31daa4ac84416e5cf38c307
Albert Llopart Navarra <d.pocafeiner@gmail.com>
2018-10-10 13:37:45 +0200

    imgui modifications
    square window, closing from window, etc

--- Project/ModuleImGui.cpp
+++ Project/ModuleImGui.cpp
@@ -30,6 +30,7 @@ bool ModuleImGui::Start()
 
 	ImGui_ImplSdlGL3_Init(App->window->GetWindowPtr());
 	
+	ImGui::GetStyle().WindowRounding = 0.0f;
 
 	return true;
 }
@@ -133,136 +134,133 @@ bool ModuleImGui::CleanUp()
 
 void ModuleImGui::ShowConfigurationMenu(bool* opened)
 {
-	static bool testing = true;
-
-	if (ImGui::Begin("Configuration"), &testing)
+	if (config_menu)
 	{
-
-		ImGui::PushItemWidth(-140);                                 
-		ImGui::Text("Options");
-		if (ImGui::CollapsingHeader("Application"))
+		ImGui::Begin("Configuration", &config_menu);
 		{
-			ImGui::InputText("App Name", TITLE, 20);
-			ImGui::InputText("Organization", "UPC CITM", 20);
-			ImGui::SliderInt("Max FPS", App->GetMaxFPS(), 0, 300);
-			PerformanceGraphCalc(App->GetFPS(), App->GetMs());
-			char title[25];
-			sprintf_s(title, 25, "Framerate %.1f", FPSvec.back());
-			ImGui::PlotHistogram("##framerate", &FPSvec.front(), FPSvec.size(), 0, title, 0.0f, 100.0f, ImVec2(310, 100));
-			sprintf_s(title, 25, "Milliseconds %0.1f", MSvec[MSvec.size() - 1]);
-			ImGui::PlotHistogram("##milliseconds", &MSvec[0], MSvec.size(), 0, title, 0.0f, 40.0f, ImVec2(310, 100));
+
+			ImGui::PushItemWidth(-140);
+			ImGui::Text("Options");
+			if (ImGui::CollapsingHeader("Application"))
+			{
+				ImGui::InputText("App Name", TITLE, 20);
+				ImGui::InputText("Organization", "UPC CITM", 20);
+				ImGui::SliderInt("Max FPS", App->GetMaxFPS(), 0, 300);
+				PerformanceGraphCalc(App->GetFPS(), App->GetMs());
+				char title[25];
+				sprintf_s(title, 25, "Framerate %.1f", FPSvec.back());
+				ImGui::PlotHistogram("##framerate", &FPSvec.front(), FPSvec.size(), 0, title, 0.0f, 100.0f, ImVec2(310, 100));
+				sprintf_s(title, 25, "Milliseconds %0.1f", MSvec[MSvec.size() - 1]);
+				ImGui::PlotHistogram("##milliseconds", &MSvec[0], MSvec.size(), 0, title, 0.0f, 40.0f, ImVec2(310, 100));
+			}
+
+			if (ImGui::CollapsingHeader("Window"))
+			{
+				if (ImGui::SliderFloat("Brightness", &App->window->brightness, 0.0f, 1.0f, "%.2f"))
+				{
+					App->window->SetBrightness(App->window->brightness);
+				}
+				if (ImGui::SliderInt("Width", &App->window->width, 1, 1920))
+				{
+					if (App->window->resizable)
+						App->window->ResizeWindow(App->window->width, App->window->height);
+				}
+				if (ImGui::SliderInt("Height", &App->window->height, 1, 1080))
+				{
+					if (App->window->resizable)
+						App->window->ResizeWindow(App->window->width, App->window->height);
+				}
+
+				if (ImGui::Checkbox("Fullscreen", &App->window->fullscreen))
+					App->window->SetFullscreen(App->window->fullscreen);
+
+				ImGui::SameLine();
+				if (ImGui::Checkbox("Resizable", &App->window->resizable))
+					App->window->SetResizable(App->window->resizable);
+
+				if (ImGui::Checkbox("Borderless", &App->window->borderless))
+					App->window->SetBorderless(App->window->borderless);
+
+				ImGui::SameLine();
+				if (ImGui::Checkbox("Full Desktop", &App->window->fulldesktop))
+					App->window->SetFullDesktop(App->window->fulldesktop);
+
+			}
+
+			if (ImGui::CollapsingHeader("Hardware"))
+			{
+				SDL_version version;
+				SDL_GetVersion(&version);
+				ImGui::Text("SDL Version:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%i.%i.%i", version.major, version.minor, version.patch);
+				ImGui::Text("OpenGL Version:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_VERSION));
+				ImGui::Text("GLSL:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_SHADING_LANGUAGE_VERSION));
+				ImGui::Separator();
+				ImGui::Text("CPUs:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%i", SDL_GetCPUCount()); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "(Cache: %ikb)", SDL_GetCPUCacheLineSize());
+				ImGui::Text("System RAM:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fGb", SDL_GetSystemRAM() / 1024.0f);
+				//ImGui::Text("Caps:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s%s%s%s%s%s%s%s%s%s%s", (SDL_Has3DNow()) ? "3DNow, " : "", (SDL_HasAVX()) ? "AVX, " : "", (SDL_HasAVX2()) ? "AVX2, " : "", (SDL_HasAltiVec()) ? "AltiVec, " : "", (SDL_HasMMX()) ? "MMX, " : "", (SDL_HasRDTSC()) ? "RDTSC, " : "", (SDL_HasSSE()) ? "SSE, " : "", (SDL_HasSSE2()) ? "SSE2, " : "", (SDL_HasSSE3()) ? "SSE3, " : "", (SDL_HasSSE41()) ? "SSE41, " : "", (SDL_HasSSE42()) ? "SSE42 " : "");
+				ImGui::Separator();
+				ImGui::Text("GPU:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_RENDERER));
+				ImGui::Text("Brand:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_VENDOR));
+
+				GLint budget;
+				GLint available;
+				GLint reserved;
+				glGetIntegerv(0x9048, &budget);
+				glGetIntegerv(0x9049, &available);
+				glGetIntegerv(0x9047, &reserved);
+				ImGui::Text("VRAM Budget:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", budget / 1024.0f);
+				ImGui::Text("VRAM Usage:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", (budget / 1024.0f) - (available / 1024.0f));
+				ImGui::Text("VRAM Available:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", available / 1024.0f);
+				ImGui::Text("VRAM Reserved:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", reserved / 1024.0f);
+			}
+			if (ImGui::CollapsingHeader("Render"))
+			{
+				ImGui::Checkbox("Wireframe Mode", &App->renderer3D->wframe);
+				if (ImGui::Checkbox("Depth Test", &App->renderer3D->depthTest))
+				{
+					App->renderer3D->SetDepthTest();
+				}
+				if (ImGui::Checkbox("Cull Face", &App->renderer3D->cullFace))
+				{
+					App->renderer3D->SetCullFace();
+				}
+				if (ImGui::Checkbox("Lighting", &App->renderer3D->lighting))
+				{
+					App->renderer3D->SetLighting();
+				}
+				if (ImGui::Checkbox("Color Material", &App->renderer3D->colorMaterial))
+				{
+					App->renderer3D->SetColorMaterial();
+				}
+				if (ImGui::Checkbox("2D Textures", &App->renderer3D->texture2D))
+				{
+					App->renderer3D->SetTexture2D();
+				}
+				ImGui::Checkbox("Show Normals", &App->renderer3D->show_normals);
+			}
 		}
-
-		if (ImGui::CollapsingHeader("Window"))
-		{
-			if (ImGui::SliderFloat("Brightness", &App->window->brightness, 0.0f, 1.0f, "%.2f"))
-			{
-				App->window->SetBrightness(App->window->brightness);
-			}
-			if (ImGui::SliderInt("Width", &App->window->width, 1, 1920))
-			{
-				if (App->window->resizable)
-					App->window->ResizeWindow(App->window->width, App->window->height);
-			}
-			if (ImGui::SliderInt("Height", &App->window->height, 1, 1080))
-			{
-				if (App->window->resizable)
-					App->window->ResizeWindow(App->window->width, App->window->height);
-			}
-
-			if (ImGui::Checkbox("Fullscreen", &App->window->fullscreen))
-				App->window->SetFullscreen(App->window->fullscreen);
-
-			ImGui::SameLine();
-			if (ImGui::Checkbox("Resizable", &App->window->resizable))
-				App->window->SetResizable(App->window->resizable);
-
-			if (ImGui::Checkbox("Borderless", &App->window->borderless))
-				App->window->SetBorderless(App->window->borderless);
-
-			ImGui::SameLine();
-			if (ImGui::Checkbox("Full Desktop", &App->window->fulldesktop))
-				App->window->SetFullDesktop(App->window->fulldesktop);
-
-		}
-		
-		if (ImGui::CollapsingHeader("Hardware"))
-		{
-			SDL_version version;
-			SDL_GetVersion(&version);
-			ImGui::Text("SDL Version:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%i.%i.%i", version.major, version.minor, version.patch);
-			ImGui::Text("OpenGL Version:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_VERSION));
-			ImGui::Text("GLSL:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_SHADING_LANGUAGE_VERSION));
-			ImGui::Separator();
-			ImGui::Text("CPUs:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%i", SDL_GetCPUCount()); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "(Cache: %ikb)", SDL_GetCPUCacheLineSize());
-			ImGui::Text("System RAM:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fGb", SDL_GetSystemRAM() / 1024.0f);
-			//ImGui::Text("Caps:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s%s%s%s%s%s%s%s%s%s%s", (SDL_Has3DNow()) ? "3DNow, " : "", (SDL_HasAVX()) ? "AVX, " : "", (SDL_HasAVX2()) ? "AVX2, " : "", (SDL_HasAltiVec()) ? "AltiVec, " : "", (SDL_HasMMX()) ? "MMX, " : "", (SDL_HasRDTSC()) ? "RDTSC, " : "", (SDL_HasSSE()) ? "SSE, " : "", (SDL_HasSSE2()) ? "SSE2, " : "", (SDL_HasSSE3()) ? "SSE3, " : "", (SDL_HasSSE41()) ? "SSE41, " : "", (SDL_HasSSE42()) ? "SSE42 " : "");
-			ImGui::Separator();
-			ImGui::Text("GPU:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_RENDERER));
-			ImGui::Text("Brand:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%s", glGetString(GL_VENDOR));
-
-			GLint budget;
-			GLint available;
-			GLint reserved;
-			glGetIntegerv(0x9048, &budget);
-			glGetIntegerv(0x9049, &available);
-			glGetIntegerv(0x9047, &reserved);
-			ImGui::Text("VRAM Budget:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", budget / 1024.0f);
-			ImGui::Text("VRAM Usage:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", (budget / 1024.0f) - (available / 1024.0f));
-			ImGui::Text("VRAM Available:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", available / 1024.0f);
-			ImGui::Text("VRAM Reserved:"); ImGui::SameLine(); ImGui::TextColored(ImVec4(0.f, 1.f, 1.f, 1.f), "%.2fMb", reserved / 1024.0f);
-		}
-		if (ImGui::CollapsingHeader("Render"))
-		{
-			ImGui::Checkbox("Wireframe Mode", &App->renderer3D->wframe);
-			if (ImGui::Checkbox("Depth Test", &App->renderer3D->depthTest))
-			{
-				App->renderer3D->SetDepthTest();
-			}
-			if (ImGui::Checkbox("Cull Face", &App->renderer3D->cullFace))
-			{
-				App->renderer3D->SetCullFace();
-			}
-			if (ImGui::Checkbox("Lighting", &App->renderer3D->lighting))
-			{
-				App->renderer3D->SetLighting();
-			}
-			if (ImGui::Checkbox("Color Material", &App->renderer3D->colorMaterial))
-			{
-				App->renderer3D->SetColorMaterial();
-			}
-			if (ImGui::Checkbox("2D Textures", &App->renderer3D->texture2D))
-			{
-				App->renderer3D->SetTexture2D();
-			}
-			ImGui::Checkbox("Show Normals", &App->renderer3D->show_normals);
-		}
+		ImGui::End();
 	}
-	ImGui::End();
+	
 }
 
 void ModuleImGui::ShowConsole()
 {
-	static bool show_console = true;
 
-	if (show_console)
+	if (showconsole)
 	{
-		ImGui::Begin("Console");
+		ImGui::Begin("Console", &showconsole);
 
-		if (ImGui::CollapsingHeader("LOGs", &showconsole))
+		if (ImGui::Button("Clear"))
 		{
-			if (ImGui::Button("Clear"))
-			{
-				consoleLogs.clear();
-			}
-			for (int i = consoleLogs.size() - 1; i >= 0; i--)
-			{
-				std::string s = consoleLogs[i];
-				ImGui::Text("%s", consoleLogs[i].c_str());
-			}
+			consoleLogs.clear();
+		}
+		for (int i = consoleLogs.size() - 1; i >= 0; i--)
+		{
+			std::string s = consoleLogs[i];
+			ImGui::Text("%s", consoleLogs[i].c_str());
 		}
 	}
-
 	ImGui::End();
 }
 
